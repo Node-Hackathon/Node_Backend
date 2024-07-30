cdaf17f2788f5abed93fcffa3b9038c20461cb96
kimys5278 <kimys5278@gmail.com>
2024-07-30 15:33:47 +0900

    CompositionPlat Random SentencesList

--- src/main/java/com/example/nodebackend/controller/FdController.java
+++ src/main/java/com/example/nodebackend/controller/FdController.java
@@ -6,7 +6,6 @@ import com.example.nodebackend.service.BlockService;
 import com.example.nodebackend.service.ComposistionService;
 import io.swagger.annotations.ApiImplicitParam;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,25 +22,32 @@ public class FdController {
     private final BlockService blockService;
     private final ComposistionService composistionService;
 
-    @ApiImplicitParam(name = "X-AUTH-TOKEN",value="로그인 성공 후 발급 받은 access_token", required = true, dataType ="String",paramType = "header")
+    @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
     @PostMapping("/block")
     public ResponseEntity<BlockResponseDto> BlockPlay(
-            @RequestPart("blockImage")MultipartFile blockImage, HttpServletRequest request) throws IOException {
+            @RequestPart("blockImage") MultipartFile blockImage, HttpServletRequest request) throws IOException {
 
-        BlockResponseDto results = blockService.BlockStacking(blockImage,request);
+        BlockResponseDto results = blockService.BlockStacking(blockImage, request);
         return ResponseEntity.status(HttpStatus.OK).body(results);
 
     }
 
-    @ApiImplicitParam(name = "X-AUTH-TOKEN",value="로그인 성공 후 발급 받은 access_token", required = true, dataType ="String",paramType = "header")
+    @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
     @PostMapping("/composition")
     public ResponseEntity<CompositionResponseDto> CompositionPlay(
-            @RequestPart("composition_image")MultipartFile composition_image, HttpServletRequest request) throws IOException {
+            @RequestPart("composition_image") MultipartFile composition_image, HttpServletRequest request) throws IOException {
 
-        CompositionResponseDto compositionResponseDto = composistionService.CompositionPlay(composition_image,request);
+        CompositionResponseDto compositionResponseDto = composistionService.CompositionPlay(composition_image, request);
         return ResponseEntity.status(HttpStatus.OK).body(compositionResponseDto);
 
     }
 
+    @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 발급 받은 access_token", required = true, dataType = "String", paramType = "header")
+    @PostMapping("/composition-sentences")
+    public ResponseEntity<String> getRandomSentence() {
 
+        String sentences = composistionService.getRandomSentence();
+        return ResponseEntity.status(HttpStatus.OK).body(sentences);
+
+    }
 }
