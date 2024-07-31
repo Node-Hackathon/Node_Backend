5dae6306bb603773a68cc85e01f5ab31f68fab1d
HwangHyeongjin <gudwlsdlsms1@naver.com>
2024-07-31 18:59:40 +0900

    .

--- src/main/java/com/example/nodebackend/data/entity/User.java
+++ src/main/java/com/example/nodebackend/data/entity/User.java
@@ -84,6 +84,11 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Composition> compositions = new ArrayList<>();
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CardGameResult> cardGameResults = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CardGameResult> NumberGameResults = new ArrayList<>();
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities(){
