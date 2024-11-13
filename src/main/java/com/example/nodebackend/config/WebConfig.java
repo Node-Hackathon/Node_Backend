64c004659969fffbc89065b260b01bbe7105c73d
Kim-Yu-Seong <107562291+kimys5278@users.noreply.github.com>
2024-11-14 03:36:15 +0900

    도메인 CORS 허용

--- src/main/java/com/example/nodebackend/config/WebConfig.java
+++ src/main/java/com/example/nodebackend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://node-service.site") // 추가된 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
